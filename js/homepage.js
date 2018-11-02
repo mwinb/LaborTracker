00402652fcfceb9e582378b174aa0419da527b9d
mwinb <mwinberry0101@gmail.com>
2018-11-02 11:03:11 -0700

    Added Geek Squad Res codes

---
+++ js/homepage.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	//Code, Front of precinct labor, Back of precinct Labor, Description
+	const rezCodes = [
+		["GSDI", 32, 30, "Diagnostic"],
+		["GSHD", 32, 18, "Hard Drive Install"],
+		["GSHW", 32, 18, "Misc Internal Part Install"],
+		["GSMM", 32, 18, "Memory Install"],
+		["GSNW", 32, 18, "Network Card/Adapter Install"],
+		["GSOI", 32, 20, "Operating System Install"],
+		["GSOS", 32, 49, "Geek Squad Operating System Install"],
+		["GSPS", 32, 18, "Power Supply Install"],
+		["GSPU", 32, 18, "Computer Processor Install"],
+		["GSSB", 32, 18, "System Board Install"],
+		["GSSC", 32, 18, "Sound Card Install"]
+		["GSSW", 32, 12, "Software Installation"],
+		["GSPW", 32, 12, "Password Reset"],
+		["GSVC", 32, 18, "Video Card Install"],
+		["GSCS", 30, 0, "PC/Tablet Setup"],
+		["GSDS", 20, 0, "Device Setup"],
+		["GSFM", 5, 0, "Restore Media Creation"],
+		["GSSH", 9, 0, "Device Screen Shield Install"],
+		["GSDB", 32, 57, "Data Back Up or Transfer"],
+		["GSRD", 32, 57, "Precinct Data Recovery Level One"],
+		["GSSF", 32, 0, "Geek Squad Express Replacement"],
+		["GSCC", 35, 0, "Geek Squad Certified Open Box Computers"],
+		["GSCP", 25, 0, "Geek Squad Certified Open Box Phones and Tablets"],
+		["GSCT", 40, 0, "Geek Squad Certified Open Box TV"],
+		["GSOB", 15, 0, "Geek Squad Open Box"],
+		["GS30", 40, 0, "30 Minute Tutorial"],
+		["GS60", 70, 0, "60 Minute Tutorial"],
+		["GSCO", 20, 0, "Geek Squad Consultation"],
+	]
+})
